b7ac24b373f7ace35e6d08728eb66b92b3f42b39
Denis D'Angelo <150398186+denisdangelo@users.noreply.github.com>
2025-02-17 18:12:46 -0300

    Update app.js
    adição de /jokenpo ào register.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // se o navegador de internet suportar este recurso
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('/jokenpo/sw.js')
         .then(() => {
             console.log("Service worker registrado!")
         })
